ec821fb6c42cfa435f7dfeadac7992f72bde59da
Kaushik <kaushiksathishks@gmail.com>
2020-08-08 15:34:07 +0530

    Updated release

--- routes/index.js
+++ routes/index.js
@@ -4,100 +4,243 @@ var router = express.Router();
 
 var admin = require('firebase-admin');
 var db = admin.database().ref();
+var stDate = ""
+var endate = ""
+var vNum = ""
+var master = ""
 
 router.get('/', function(req, res, next) {
-    var morning = false;
-    var a = false;
-    var e = false;
-    var d = new Date();
-    var dd = d.getDate();
-    var m = d.getMonth() + 1;
-    var y = d.getFullYear();
-    var date = dd + '-' + m + '-' + y;
+    res.render('login');
+});
+
+router.post('/', function(req, res, next) {
+    var name = req.body.username;
+    var pwd = req.body.pass;
+
+    if (name.toLowerCase() == "balaji") {
+        if (pwd == "balajitransports") {
+            master = "success";
+            res.redirect('/secured/home/')
+        } else {
+            res.send("Invalid Password");
+        }
+    } else {
+        res.send("Invalid Username");
+    }
+});
+
+router.get('/secured/home/', function(req, res, next) {
+    if (master == "success") {
+        var d = new Date();
+        var dd = d.getDate();
+        var m = d.getMonth() + 1;
+        var y = d.getFullYear();
+        if (dd < 10) {
+            dd = '0' + dd;
+        }
+        if (m < 10) {
+            m = '0' + m;
+        }
+        var date = dd + '-' + m + '-' + y;
+        var map = {}
+        var details = {}
+        var flag = 0;
+        var info = db.child("Daily");
+        info.once("value", function(snapshot) {
+            if (snapshot.val() != null) {
+                map = snapshot.val();
+                map = map[date];
+                if (map == null) {
+                    res.render('index', { title: 'Details', date: date, info: null, empty: true, details: false });
+                } else {
+                    res.render('index', { title: 'Details', date: date, info: map, empty: false, details: true });
+                }
+            } else {
+                res.render('index', { title: 'Details', date: date, info: null, empty: true, details: false });
+            }
+        });
+    } else {
+        res.redirect('/');
+    }
+
+});
+
+router.post('/secured/home/', function(req, res, next) {
+    if (master == "success") {
+        var search = req.body.search.toLowerCase()
+        var d = new Date();
+        var dd = d.getDate();
+        var m = d.getMonth() + 1;
+        var y = d.getFullYear();
+        if (dd < 10) {
+            dd = '0' + dd;
+        }
+        if (m < 10) {
+            m = '0' + m;
+        }
+        var date = dd + '-' + m + '-' + y;
+        var map = {}
+        var details = {}
+        var flag = 0;
+        var info = db.child("Daily");
+        info.once("value", function(snapshot) {
+            if (snapshot.val() != null) {
+                map = snapshot.val();
+                map = map[date];
+                if (map == null) {
+                    res.render('index', { title: 'Details', date: date, info: null, empty: true, details: false });
+                } else {
+                    for (var key in map) {
+                        if (key.toLowerCase().includes(search)) {
+                            details[key] = map[key]
+                        } else {
+                            for (var k in map[key]) {
+                                if (map[key][k].mechanicName.toLowerCase().includes(search) || map[key][k].description.toLowerCase().includes(search)) {
+                                    details[key] = map[key]
+                                }
+                            }
+                        }
+                    }
+                    res.render('index', { title: 'Details', date: date, info: details, empty: false, details: true });
+                }
+            } else {
+                res.render('index', { title: 'Details', date: date, info: null, empty: true, details: false });
+            }
+        });
+    } else {
+        res.redirect('/')
+    }
+});
+
+
+router.get('/secured/home/:date/:vehicle', function(req, res, next) {
+    var date = req.params.date
+    var Num = req.params.vehicle
+    var map = {}
+    var info = db.child("Daily");
+    info.once("value", function(snapshot) {
+        map = snapshot.val();
+        map = map[date][Num];
+        res.render('view', { title: 'Details', num: Num, info: map });
+    });
+});
+
+
+router.get('/logged/details/', function(req, res, next) {
+    res.render('filter', { title: "Details", details: false, empty: false, sdate: null, edate: null, dates: false, vehicle: false, date: false });
+});
+
+router.get('/logged/dates/', function(req, res, next) {
+    res.render('filter', { title: "Details", details: false, empty: false, sdate: null, edate: null, dates: true, vehicle: false, date: false });
+});
+
+router.get('/logged/vehicle/', function(req, res, next) {
+    res.render('filter', { title: "Details", details: false, empty: false, sdate: null, edate: null, dates: false, vehicle: true, date: false });
+});
+
+
+router.post('/logged/dates/', function(req, res, next) {
+    if (req.body.stDate != null) {
+        stDate = req.body.stDate
+        endate = req.body.endDate
+    } else {
+        search = req.body.search.toLowerCase()
+    }
+    var sdate = stDate.split("-");
+    var edate = endate.split("-")
+    var sd = new Date()
+    sd.setDate(sdate[2])
+    sd.setMonth((sdate[1]) - 1)
+    sd.setFullYear(sdate[0])
+    var sDate = sdate[2] + '/' + sdate[1]
+    var ed = new Date()
+    ed.setDate(edate[2])
+    ed.setMonth((edate[1]) - 1)
+    ed.setFullYear(edate[0])
+    var eDate = edate[2] + '/' + edate[1]
     var map = {}
     var details = {}
-    var today = new Date();
-    var day = today.getDate();
-    var month = today.getMonth() + 1;
-    var year = today.getFullYear();
+    info = {}
     var flag = 0;
     var info = db.child("Daily");
     info.once("value", function(snapshot) {
         if (snapshot.val() != null) {
             map = snapshot.val();
-            for (var key in map) {
-                var d = key.split("-");
-                if (d[0] == day && d[1] == month && d[2] == year) {
-                    details[key] = map[key];
-                    flag = 1;
-                    break;
-                }
-            }
-            if (flag = 0) {
-                res.render('index', { title: 'Details', date: date, info: null, morning: false, afternoon: false, evening: false, empty: true });
+            if (map == null) {
+                res.render('filter', { title: "Details", details: false, empty: true, sdate: null, edate: null, dates: true, vehicle: false, date: false });
             } else {
-                for (var key in details) {
-                    for (var k in details[key]) {
-                        if (details[key][k]["Morning"] != null) {
-                            morning = true;
-                        }
-                        if (details[key][k]["Afternoon"] != null) {
-                            a = true;
-                        }
-                        if (details[key][k]["Evening"] != null) {
-                            e = true;
+                for (var key in map) {
+                    details[key] = {}
+                    var j = key.split("-")
+                    var k = new Date()
+                    k.setDate(j[0])
+                    k.setMonth((j[1]) - 1)
+                    k.setFullYear(j[2])
+                    if (k.getTime() >= sd.getTime() && k.getTime() <= ed.getTime()) {
+                        if (req.body.search != null) {
+                            for (var vnum in map[key]) {
+                                if (vnum.toLowerCase().includes(search)) {
+                                    details[key][vnum] = map[key][vnum]
+                                } else {
+                                    for (var m in map[key][vnum]) {
+                                        if (map[key][vnum][m].mechanicName.toLowerCase() == search) {
+                                            details[key][vnum] = map[key][vnum]
+                                        }
+                                    }
+                                }
+                            }
+                        } else {
+                            details[key] = map[key]
                         }
                     }
-                    break;
                 }
-                res.render('index', { title: 'Balaji Transports', date: date, info: details, morning: morning, afternoon: a, evening: e, empty: false });
+                if (Object.keys(details).length != 0) {
+                    res.render('filter', { title: "Details", details: true, empty: false, sdate: sDate, edate: eDate, dates: false, vehicle: false, date: true, info: details });
+                } else {
+                    res.render('filter', { title: "Details", details: false, empty: true, sdate: null, edate: null, dates: true, vehicle: false, date: false });
+                }
             }
         } else {
-            res.render('index', { title: 'Balaji Transports', date: date, info: null, morning: false, afternoon: false, evening: false, empty: true });
+            res.render('filter', { title: "Details", info: null, details: false, empty: true, sdate: null, edate: null });
         }
     });
 });
 
-router.get('/logged/details/', function(req, res, next) {
-    res.render('view', { title: "Details", dates: false, mechanic: false, vehicle: false, info: null, empty: false, vname: null, mname: null, sdate: null, edate: null });
-});
 
-router.get('/logged/details/date', function(req, res, next) {
-    res.render('view', { title: "Details", dates: true, mechanic: false, vehicle: false, info: null, empty: false, vname: null, mname: null, sdate: null, edate: null });
-});
-
-router.get('/logged/details/mechanic', function(req, res, next) {
-    res.render('view', { title: "Details", dates: false, mechanic: true, vehicle: false, info: null, empty: false, vname: null, mname: null, sdate: null, edate: null });
-});
-
-router.post('/logged/details/mechanic', function(req, res, next) {
+router.post('/logged/vehicle/', function(req, res, next) {
     var map = {}
-    var info = {}
     var details = {}
     var flag = 0;
-    var mname = req.body.mname;
-    mname = mname.toLowerCase();
-    var minfo = db.child("Mechanic");
+    if (req.body.search == null) {
+        vNum = req.body.vehicleNum.toLowerCase();
+    } else {
+        var mname = req.body.search.toLowerCase()
+    }
     var vinfo = db.child("Daily");
-    minfo.once("value", function(snapshot) {
+    vinfo.once("value", function(snapshot) {
         if (snapshot.val() != null) {
             map = snapshot.val();
             for (var key in map) {
-                if (key.toLowerCase() == mname) {
-                    for (var k in map[key]) {
-                        details[k] = map[key][k];
+                details[key] = {}
+                for (var k in map[key]) {
+                    if (k.toLowerCase().includes(vNum)) {
+                        if (req.body.search != null) {
+                            for (var m in map[key][k])
+                                if (mname == map[key][k][m].mechanicName.toLowerCase())
+                                    details[key][k] = map[key][k]
+                        } else {
+                            details[key][k] = map[key][k]
+                        }
                     }
-                    flag = 1;
-                    break;
                 }
             }
-            if (flag == 0) {
-                res.send("Mechanic not found");
+            if (Object.keys(details).length != 0) {
+                res.render('filter', { title: "Details", details: true, empty: false, sdate: null, edate: null, dates: false, vehicle: true, date: false, info: details });
             } else {
-                res.render('view', { title: "Mechanic Details", dates: false, mechanic: true, vehicle: false, info: details, empty: false, vname: null, mname: mname, sdate: null, edate: null });
+                res.render('filter', { title: "Details", details: false, empty: true, sdate: null, edate: null, dates: true, vehicle: false, date: false });
             }
         } else {
-            res.render('view', { title: "Mechanic Details", dates: false, mechanic: true, vehicle: false, info: null, empty: true, vname: null, mname: mname, sdate: null, edate: null });
+            res.render('filter', { title: "Details", details: false, empty: true, sdate: null, edate: null, dates: true, vehicle: false, date: false });
         }
     });
 });
